daeddcfdcb90e4077140f76ce4052cc13b040fe8
Aftermath <From_JingWANG@outlook.com>
2024-04-10 00:04:22 +0800

    modify 配置端口

--- vite.config.ts
+++ vite.config.ts
@@ -36,5 +36,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  //修改启动的
+  server: {
+    host: '0.0.0.0', // 这个用于启动
+    port: 8092, // 指定启动端口
+    open: true //启动后是否自动打开浏览器
   }
 })
